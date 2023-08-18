e28953939ae96e0005fadd9a95dc032d66fabd33
maxvakh2002 <137182864+maxvakh2002@users.noreply.github.com>
2023-08-18 11:13:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,39 +9,3 @@ headers.forEach(function (item) {
 function showContent() {/*при нажатии на кнопку программа отображает соседний с заголовком элемент - блок с текстом*/
     this.nextElementSibling.classList.toggle('hidden');
 }
-
-/*настройка появления кнопки*/
-
-jQuery(document).ready(function($){
-    
-    $('#btnarrow').fadeOut(); //скрываем кнопку
-
-    $(window).scroll(function () {
-        
-            if ($(this).scrollTop() > 500) { // при прокрутке страницы на 500 пикселей вниз
-                $('#btnarrow').fadeIn();   // отображаем кнопку
-            } 
-        
-            else {
-                $('#btnarrow').fadeOut(); // в противном случае скрываем
-            }
-        
-    });/*function-scroll*/
-    
-});/*function-ready*/
-
-/*при нажатии на кнопку возвращаемся вверх сайта*/
-
-const goTopSubtitle = document.querySelector('.jsarrow');
-
-goTopSubtitle.addEventListener("click", goTop);
-
-function goTop(){
-    
-    if (window.pageYOffset > 0){
-        
-        window.scrollBy(0, -75);
-        setTimeout(goTop, 0);
-        
-    }
-}
